a49cb6c3556f9701ac90cf3feb4878534c5e62c8
Candidat-005 <g.thomas@laposte.net>
2024-03-05 12:10:23 +0100

    first comments

--- src/components/PokemonCard/PokemonCollection.tsx
+++ src/components/PokemonCard/PokemonCollection.tsx
@@ -11,23 +11,22 @@ function PokemonCollection(props: Props) {
 
   return (
     <div>
-        <div className="font-bold text-4xl text-center p-7">
-          Pokedex
-        </div>
+      <div className="font-bold text-4xl text-center p-7">
+        Pokedex
+      </div>
       <div className="flex flex-wrap justify-center gap-10">
         {pokemons.map((pokemon) => (
-          <div className="border border-black w-fit h-fit">
+          <div key={pokemon.id} className="border border-black w-fit h-fit">
             <PokemonList
-              key={pokemon.id}
               name={pokemon.name}
               id={pokemon.id}
               image={pokemon.sprites.front_default}
               type={pokemon.types[0].type.name}
             />
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
   );
 }
 
